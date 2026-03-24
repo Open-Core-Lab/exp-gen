04af2febe7a4afdd53335c1ba17c9a20de978b8d
MadhushaPrasad <madushaprasad21@gmail.com>
2026-03-24 11:38:39 +0530

    chore: ts none db project route

--- src/templates/ts/none/src/routes/api.route.ts
+++ src/templates/ts/none/src/routes/api.route.ts
@@ -1,9 +1,9 @@
 import { Router } from "express";
-import type { Request,Response } from "express";
+import type { Request, Response } from "express";
 
 const router = Router();
 
-router.get("/", async (req:Request, res:Response) => {
+router.get("/", async (req: Request, res: Response) => {
   res.send({ message: "Ok api is working 🚀" });
 });
 
