07c32022e0660ae200eb2a8cef48a3d83a575e93
MadhushaPrasad <madushaprasad21@gmail.com>
2026-03-24 11:37:12 +0530

    chore: move mongodb setup project to folder and create none db ts project

---
+++ src/templates/ts/mongodb/src/repositories/user.repository.ts
@@ -0,0 +1,22 @@
+import { User } from "../models/user.model.js";
+import { CreateUserDTO, UpdateUserDTO } from "../dtos/user.dto.js";
+
+export const getAllUsers = async () => {
+  return await User.find({});
+};
+
+export const getUserById = async (id: string) => {
+  return await User.findById(id);
+};
+
+export const createUser = async (data: CreateUserDTO) => {
+  return await User.create(data);
+};
+
+export const updateUser = async (id: string, data: UpdateUserDTO) => {
+  return await User.findByIdAndUpdate(id, data, { new: true });
+};
+
+export const deleteUser = async (id: string) => {
+  return await User.findByIdAndDelete(id);
+};
